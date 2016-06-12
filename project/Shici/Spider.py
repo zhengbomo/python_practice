ef070c7f9d691cb45e4cf916fdc053f3db3fb409
zhengbomo <zhengbomo@hotmail.com>
2016-06-12 21:28:38 +0800

    add pycharm setting file

--- project/Shici/Spider.py
+++ project/Shici/Spider.py
@@ -2,4 +2,33 @@
 # -*- coding:utf-8 -*-  
 __author__ = 'bomo'
 
-# 爬取Url入库
+from SpiderMain import SpiderMain
+
+if __name__ == '__main__':
+    spider = SpiderMain()
+
+    # 1. 爬取诗词列表
+    # spider.poem_list_crew()
+    # count = spider.db.url_count()
+    # errors = spider.db.select_error(range(1, 4))
+    # print '诗词列表爬取完成,总共%d条数据, 错误%d条' % (count, len(errors))
+
+    # 2. 爬取诗词详情
+    # spider.poem_detail_crew()
+    # count = spider.db.poem_count()
+    # errors = spider.db.select_error(range(4, 6))
+    # print '诗词详情爬取完成,总共%d条数据, 错误%d条' % (count, len(errors))
+
+    # 3. 爬取诗人详情
+    # spider.poem_author_crew()
+    # count = spider.db.infomation_count()
+    # errors = spider.db.select_error(range(6, 8))
+    # print '诗词相关数据爬取完成,总共%d条数据, 错误%d条' % (count, len(errors))
+
+    # 4. 爬去相关内容
+    # spider.infomation_crew()
+    # count = spider.db.infomation_count()
+    # errors = spider.db.select_error(range(8, 10))
+    # print '诗词相关数据爬取完成,总共%d条数据, 错误%d条' % (count, len(errors))
+
+    print 'completed'
--- project/Shici/SpiderMain.py
+++ project/Shici/SpiderMain.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
-import thread
-
-__author__ = 'bomo'
 
 from Downloader import Downloader
 from Analyzer import Analyzer
 from SpiderDb import SpiderDb
+import thread
 
 
 class SpiderMain(object):
@@ -169,32 +167,3 @@ class SpiderMain(object):
             else:
                 # 没有了
                 return
-
-if __name__ == '__main__':
-    spider = SpiderMain()
-
-    # 1. 爬取诗词列表
-    # spider.poem_list_crew()
-    # count = spider.db.url_count()
-    # errors = spider.db.select_error(range(1, 4))
-    # print '诗词列表爬取完成,总共%d条数据, 错误%d条' % (count, len(errors))
-
-    # 2. 爬取诗词详情
-    # spider.poem_detail_crew()
-    # count = spider.db.poem_count()
-    # errors = spider.db.select_error(range(4, 6))
-    # print '诗词详情爬取完成,总共%d条数据, 错误%d条' % (count, len(errors))
-
-    # 3. 爬取诗人详情
-    # spider.poem_author_crew()
-    # count = spider.db.infomation_count()
-    # errors = spider.db.select_error(range(6, 8))
-    # print '诗词相关数据爬取完成,总共%d条数据, 错误%d条' % (count, len(errors))
-
-    # 4. 爬去相关内容
-    spider.infomation_crew()
-    count = spider.db.infomation_count()
-    errors = spider.db.select_error(range(8, 10))
-    print '诗词相关数据爬取完成,总共%d条数据, 错误%d条' % (count, len(errors))
-
-    print 'completed'
